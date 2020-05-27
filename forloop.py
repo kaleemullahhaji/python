d0778d88982758bf40dd388cadf6549d2e52996d
kaleemullahhaji <49298383+kaleemullahhaji@users.noreply.github.com>
2020-05-27 08:44:57 +0300

    foorloop

---
+++ forloop.py
@@ -0,0 +1,14 @@
+subjects = ['Arabic','English','Math']
+#print(subjects)
+#print(subjects[1])
+subjects.append('Hestory')
+#print(subjects)
+#subjects.pop()
+#print(subjects)
+#subjects.pop(0)
+#print(subjects)
+
+
+for x in subjects:
+    print(x)
+    
