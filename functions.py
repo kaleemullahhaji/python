823415f4cdd76fb1fe4889e2d1a885518da486e5
kaleemullahhaji <49298383+kaleemullahhaji@users.noreply.github.com>
2020-05-27 09:35:11 +0300

    functions

---
+++ functions.py
@@ -0,0 +1,38 @@
+def liner():
+    print('---------------------')
+
+
+def addition(num1,num2):
+    print(int(num1)+int(num2))
+
+
+def sub(num1,num2):
+    print(num1-num2)
+
+
+def mult(num1,num2):
+    print(num1*num2)
+
+
+def div(num1,num2):
+    print(num1/num2)
+
+
+n1 = int(input("Enter your first number : "))
+n2 = int(input("Enter your second number : "))
+
+addition(n1,n1)
+liner()
+sub(n1,n2)
+liner()
+mult(n1,n2)
+liner()
+div(n1,n2)
+
+
+
+
+#print('kaleem')
+#iner()
+#print(25)
+#liner()
